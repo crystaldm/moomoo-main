f2514b566caa3c7e571e2b3809765301aedaab50
student <student@administrators-iMac-89.local>
2015-10-14 14:14:30 -0700

    week02

---
+++ game/js/cow.js
@@ -0,0 +1,61 @@
+// main script for moo moo mania
+
+var cow = (function() {
+  var scriptQueue = [],
+      numResourcesLoaded = 0,
+      numResources = 0,
+      executeRunning = false;
+
+  function executeScriptQueue() {
+    var next = scriptQueue[0],
+        first, script;
+    if(next && next.loaded) {
+      executeRunning = true;
+      scriptQueue.shift();
+      first = document.getElementsByTagName("script")[0];
+      script = document.createElement("script");
+      script.onload = function() {
+        if(next.callback) {
+          next.callback();
+        }
+        executeScriptQueue();
+      };
+      script.src = next.src;
+      first.parentNode.insertBefore(script, first);
+    } else {
+      executeRunning = false;
+    }
+  }
+
+  function load(src, callback) {
+    var image, queueEntry;
+    numResources++;
+
+    queueEntry = {
+      src: src,
+      callback: callback,
+      loaded: false
+    };
+    scriptQueue.push(queueEntry);
+
+    image = new Image();
+    image.onload = image.onerror = function() {
+      numResourcesLoaded++;
+      queueEntry.loaded = true;
+      if(!executeRunning) {
+        executeScriptQueue();
+      }
+    };
+    image.src = src;
+  }
+
+  function setup() {
+    console.log("in::cow.setup");
+  }
+
+  return {
+    load: load,
+    setup: setup
+  }
+
+}) ();
---
+++ game/js/dom.js
@@ -0,0 +1,31 @@
+// dom.js
+// dom helpers
+cow.dom = (function() {
+  function $(path, parent) {
+    parent = parent || document;
+    return parent.querySelectorAll(path);
+  }
+
+  function hasClass(el, clsName) {
+    var regex = new RegExp("(^|\\s)" + clsName + "(\\s|$)");
+    return rege.test(el.className);
+  }
+
+  function addClass(el, clsName) {
+    if(!hasClass(el, clsName)) {
+      el.className += " " + clsName;
+    }
+  }
+
+  function removeClass(el, clsName) {
+    var regeX = new RegExp ("(^|\\s)" + clsName + "(\\s|$)");
+    el.className = el.className.replace(regex, " ");
+  }
+
+  return {
+    $: $,
+    hasClass: hasClass,
+    addClass: addClass,
+    removeClass: removeClass
+  };
+}) ();
